a7fee5331002e1b82c8b39856e4826af89e18e1c
izlude <izlude.k@gmail.com>
2012-03-24 10:32:58 +0900

    first commit

---
+++ FizzBuzzTest.cpp
@@ -0,0 +1,59 @@
+#include "FizzBuzz.h"
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace CPPUNIT_NS;
+
+class FizzBuzzTest : public TestFixture {
+  CPPUNIT_TEST_SUITE( FizzBuzzTest );
+  
+  /* ここから、テストケースを設定 */
+  CPPUNIT_TEST( testFizz );
+  CPPUNIT_TEST( testBuzz );
+  CPPUNIT_TEST( testFizzBuzz );
+  CPPUNIT_TEST( testOther );
+  /* ここまで */
+  
+  CPPUNIT_TEST_SUITE_END();
+
+  FizzBuzz* fb_;
+  void testFizz();
+  void testBuzz();
+  void testFizzBuzz();
+  void testOther();
+
+public:
+  void setUp() {
+    this->fb_ = new FizzBuzz();
+  }
+  void tearDown() {
+    delete this->fb_;
+  }
+};
+
+/* ここから、テストケースを記述 */
+void FizzBuzzTest::testFizz()
+{
+    std::string s = "Fizz";
+    CPPUNIT_ASSERT_EQUAL(s, fb_->Print(3));
+}
+
+void FizzBuzzTest::testBuzz()
+{
+    std::string s = "Buzz";
+    CPPUNIT_ASSERT_EQUAL(s, fb_->Print(5));
+}
+
+void FizzBuzzTest::testFizzBuzz()
+{
+    std::string s = "FizzBuzz";
+    CPPUNIT_ASSERT_EQUAL(s, fb_->Print(15));
+}
+
+void FizzBuzzTest::testOther()
+{
+    std::string s = "2";
+    CPPUNIT_ASSERT_EQUAL(s, fb_->Print(2));
+}
+/* ここまで、テストケースの記述 */
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FizzBuzzTest);
---
+++ TestMain.cpp
@@ -0,0 +1,29 @@
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/CompilerOutputter.h>
+
+using namespace CPPUNIT_NS;
+
+int main(int argc, char **argv) {
+  /* テストコントローラの生成 */
+  TestResult controller;
+  TestResultCollector result;
+  controller.addListener(&result);
+
+  /* 進行状況を「.」で表示する */
+  BriefTestProgressListener progress;
+  controller.addListener(&progress);
+
+  /* テストを追加して、テストの実行 */
+  TestRunner runner;
+  runner.addTest(TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  CompilerOutputter outputter(&result, stdCOut());
+  outputter.write();
+
+  return result.wasSuccessful() ? 0 : 1;
+}
